8b74e453020e39e9cfbfd4b7b01663475abe1968
Inna Ilyaev <inna4ilyaev@gmail.com>
2021-04-12 14:35:39 +0300

    12/04/21#8

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const http = require('http');
+const Url = require('Url');
+const usersJson = require('./users.json');
 const port = 8000;
 
-const users = [
-    { name: "Itay",id:1},
-    { name:"Evgeni",id:2},
-    { name:"Star",id:3}]
-
 const server = http.createServer((req,res)=>{
+    q=Url.parse(req.url,true).query;
+    console.log(q);
+
     if(req.method==='GET'){
         if(req.url === '/'){
             res.write("Hello");
         }
-        if(req.url === '/getAll'){
-            res.write(JSON.stringify(users));
+        if(req.url === '/users'){
+            res.write(JSON.stringify(usersJson.users));
+        }
+        else if(req.url.includes('user')){
+            if(q.id)
+                res.write(JSON.stringify(usersJson.users[q.id]));
+            else
+            res.write(JSON.stringify(usersJson.users.filter((user)=>{
+                return user.capsule === parseInt(q.capsule)
+            })));
+
+            
         }
     }
     res.end();
