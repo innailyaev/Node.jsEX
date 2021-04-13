c3408fa1e04f5b17e607bac5f10046fbb2c740aa
Inna Ilyaev <inna4ilyaev@gmail.com>
2021-04-13 23:28:20 +0300

    13/04/21#3

--- API/api.js
+++ API/api.js
@@ -1,12 +1,20 @@
-const axios = require('axios')
+const axios = require('axios');
+const request = require('request');
 
-const getBreeds = async () => {
-    try {
-        const data = await axios.get('https://dog.ceo/api/breeds/list/all');
-            return data;
-    } catch (error) {
-      console.error(error)
-    }
-  }
 
-  console.log(getBreeds());
+const getBreeds = () => {
+   
+       axios.get('https://dog.ceo/api/breeds/list/all').then(
+            result=>{console.log("Result",result);}
+        ).catch (error =>{
+            console.log(error)
+        })
+      
+}
+  
+
+  getBreeds();
+
+    request({url:'https://dog.ceo/api/breeds/list/all', json:true}, function (error, response, body) {
+        console.log(response.body);
+});
