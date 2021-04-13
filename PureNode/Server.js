2e9681d69931f0b54ce4de3bc7113ef0bac61812
Inna Ilyaev <inna4ilyaev@gmail.com>
2021-04-14 00:46:13 +0300

    14/04/21#5

--- PureNode/Server.js
+++ PureNode/Server.js
@@ -10,7 +10,7 @@ http.createServer((req, res) => {
     if (req.method === "GET") {
 
         res.writeHead(200,{ 'Content-Type': 'text/plain' })
-     a
+     
         switch(req.url){
             case "/raw-html":
                 res.write('<h1> Hello, my name is inna </h1>');
